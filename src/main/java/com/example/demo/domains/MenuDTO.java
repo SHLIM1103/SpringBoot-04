79bbe0b21ef8a49dbf643512747144172b53d021
SHLIM1103 <gusl5525@gmail.com>
2020-12-11 12:05:53 +0900

    2020-12-11 12:00

---
+++ src/main/java/com/example/demo/domains/MenuDTO.java
@@ -0,0 +1,8 @@
+package com.example.demo.domains;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MenuDTO {
+    private String product, price, review, interest, cart;
+}
